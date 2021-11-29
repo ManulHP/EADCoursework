c4f7f65bf5d4b9811e2d4caf6c6c04b1d1f14057
ManulHP <manulhpallege@gmail.com>
2021-11-29 12:16:27 +0530

    initial commit

---
+++ Coursework/Coursework/Data/Project/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coursework.Data.Project
+{
+    public class Project
+    {
+        [Key]
+        public Int32 Id { get; set; }
+        [StringLength(55)]
+        public String Title { get; set; }
+        [StringLength(55)]
+        public String CompanyName { get; set; }
+    }
+}
